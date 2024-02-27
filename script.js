06656c8e8050f4d388d7641baf05a53edd8d9069
unknown <you@example.com>
2024-02-26 20:58:42 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var elemc = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemc.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+  var img = e.getAttribute("data-image")
+  fixed.style.backgroundImage = `url(${img}`
+    })
+})
